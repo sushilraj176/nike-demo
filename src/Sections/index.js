60e7f0e201781eeb086d230042ed73cda0eac624
Sushil Raj <sushil.raj@capwisefin.com>
2023-08-20 22:38:58 +0530

    homepage nike

---
+++ src/Sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import SpecialOffers from "./SpecialOffers";
+
+export {
+  Hero,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+  PopularProducts,
+  CustomerReviews,
+  Footer,
+};
